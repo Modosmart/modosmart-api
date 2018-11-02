78bfbb7e2cfb0279230ece8ac8704e1618309a06
Mohamed ElSabagh <mohamed.elsabagh88@gmail.com>
2018-11-02 16:48:28 +0000

    fixed refresh resources

--- refresh_resources.py
+++ refresh_resources.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-while true:
+while True:
     time.sleep(1800) # wait for 30 minutes
     response = requests.get('http://localhost:3030/api/scan')
     print response
